13f1ded8023b1b7f07d3b195d50ea27350538f90
Jorge Rodriguez <alkanware@gmail.com>
2023-03-13 09:52:55 -0500

    feat: added semantic-versioning

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,3 @@ export { Header } from './stories/Header';
 export { MyLabel } from './components/MyLabel';
 export { Page } from './stories/Page';
 export { Button } from './stories/Button';
-
--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 
 import { Header } from './Header';
 import './page.css';
 
-interface PageProps {
+export interface PageProps {
   user?: {};
   onLogin: () => void;
   onLogout: () => void;
